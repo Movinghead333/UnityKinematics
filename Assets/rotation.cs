6d7c0227f08cd7f383300db70994923b4b05c9ff
Sven Müller <movinghead333@googlemail.com>
2019-10-23 23:54:23 +0200

    first commit

---
+++ Assets/rotation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotation : MonoBehaviour
+{
+    public Transform transform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 position = new Vector3(0, 1.5f, 0);
+        Vector3 axis = new Vector3(1f, 0, 0);
+
+        transform.RotateAround(position, axis, Time.deltaTime * 20);
+    }
+}
